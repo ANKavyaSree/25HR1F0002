fb715e8285a51ed43599df313ea655279f0fda94
ANKavyaSree <ankavyasri11@gmail.com>
2026-06-27 13:09:35 +0530

    Stage 2

--- notifications/views.py
+++ notifications/views.py
@@ -1,7 +1,16 @@
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+from .models import Notification
+from .serializers import NotificationSerializer
 
 
 @api_view(["GET"])
 def health_check(request):
     return Response({"status": "ok"})
+
+
+class NotificationViewSet(viewsets.ModelViewSet):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
